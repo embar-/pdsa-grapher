b90d522ea883f55959cc252e027e8f21a27aa2d5
embar- <opensuse.lietuviu.kalba@gmail.com>
2025-01-09 09:07:10 +0200

    Funkcijos aprašymo papildymas

--- locale_utils/translations.py
+++ locale_utils/translations.py
@@ -6,15 +6,19 @@ Vertimai gaunami kviečiant funkcijas „_()“ ir, jei reikia atsižvelgti į k
 from gettext import translation
 
 
-def set_gettext_locale(lang="lt"):
-    # gettex naudojamos kalbos atnaujinimas, kuris diegiamas globaliai naudojimui su _()
+def set_gettext_locale(lang: str="lt"):
+    """
+    gettex naudojamos kalbos atnaujinimas, kuris diegiamas globaliai naudojimui su _()
+    :param lang: pasirinktos kalbos kodas
+    :return: GNUTranslations objektas
+    """
     lang_trans = translation("pdsa-grapher", 'locale', languages=[lang])
     lang_trans.install()
     return lang_trans
 
 
 # Apsirašyti savo pgettext vietoj gettext.pgettext, nes jo neatnaujina gettext .install()
-def pgettext(context, message):
+def pgettext(context: str, message: str) -> str:
     """
     Pranešimo vertimas atsižvelgiant į pranešimo kontekstas. Daro tą patį kaip gettext.pgettext,
     tačiau pastarasis netinka tuomet, kai keičiame kalbas programos viduje.
