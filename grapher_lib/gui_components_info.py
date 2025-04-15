8c3ff321297767d60c867b33abfb6b07bd0641e2
mindaugasbar <Mindaugas.Baranauskas@stat.gov.lt>
2025-04-15 15:22:27 +0300

    Instrukcijų atnaujinimas ir papildymas

--- grapher_lib/gui_components_info.py
+++ grapher_lib/gui_components_info.py
@@ -21,27 +21,38 @@ import dash_bootstrap_components as dbc
 
 def graph_usage_info():
     grafikas_content = dbc.Card(
-        dbc.CardBody(
-            [
-                html.H6(_("Graph usage instructions"), className="card-title"),
-                html.Div(
-                    children=[
-                        html.P(
-                            _("The graph shows the relationships between the selected tables, "
-                              "which are described in the references file.")
+        dbc.CardBody([
+            html.H6(_("Graph usage instructions"), className="card-title"),
+            html.Div(
+                children=[
+                    html.P(
+                        _("The graph shows the selected tables as nodes and their relationships as lines. ") +
+                        _("Relationships are shown if they are described in the references file.")
+                    ),
+                    html.P(_("The graph is interactive:")),
+                    html.Ul([
+                        html.Li(
+                            _("Double-clicking on a node will display the table's metadata in a tooltip: ") +
+                            _("columns, their descriptions, displayed relations, and non-displayed relations.")
                         ),
-                        html.P(_("The graph is interactive:")),
-                        html.Ul(
-                            children=[
-                                html.Li(_("You can drag points.")),
-                                html.Li(_("View can be zoomed in/out and panned.")),
-                            ],
-                        ),
-                    ],
-                    className="card-text",
-                ),
-            ]
-        ),
+                        html.Li(_("Hold the Ctrl key and left-click on nodes to select multiple tables.")),
+                        html.Li(_("Change filter of drawable tables using keyboard based on the selection made with the mouse:")),
+                        html.Ul([
+                            html.Li([html.B(_("Delete")), " – ", _("to remove,")]),
+                            html.Li([html.B(_("Enter")), " – ", _("to keep only selected tables,")]),
+                            html.Li([
+                                html.B("P"), _(" or "), html.B("+"), " – ",
+                                _("to append selected tables (e.g. displayed neighbors).")
+                            ]),
+                        ]),
+                        html.Li(_("You can move selected nodes by left-clicking on one of them and dragging.")),
+                        html.Li(_("The entire graph can be panned by dragging the empty area with the left mouse button.")),
+                        html.Li(_("The view can be zoomed in or out using the mouse wheel.")),
+                    ]),
+                ],
+                className="card-text",
+            ),
+        ]),
     )
     grafikas = html.Div(
         [
@@ -57,7 +68,7 @@ def graph_usage_info():
                 target="tutorial-grafikas-legacy-target",
                 body=True,
                 trigger="legacy",
-                style={"fontSize": "90%"},
+                style={"fontSize": "90%", "minWidth": "450px"},
                 placement="left-start",
             ),
         ]
@@ -74,56 +85,51 @@ def filters_usage_info():
                 html.Div(
                     children=[
                         html.P(
-                            _("The graph can be displayed by selecting individual tables or by entering "
-                              "a list of tables. Information about the tables can also be found here")
+                            _("The graph view depends on your table selection and other configurations. "),
+                            _("For the initial view, up to 10 tables can be automatically preselected " +
+                              "based on their metadata and the number of relations - you can change it manually."),
                         ),
-                        html.P(
-                            children=[
-                                html.B(_("Graph engine")), ", ", html.B(_("Layout")), " - ",
-                                html.Label(_("Style of the point placement in the graph.")),
-                            ]
-                        ),
-                        html.P(
-                            children=[
-                                html.B(_("Select tables to graph")), " - ",
-                                html.Label(
-                                    _("Select PDSA tables which relationships you want to display")
-                                ),
-                            ]
-                        ),
-                        html.P(
-                            children=[
-                                html.B(_("Add list of tables")), " - ",
-                                html.Label(
-                                    _("You can also specify the tables to be displayed in the list; "
-                                      "the names of the tables must be separated by commas")
-                                ),
-                            ]
-                        ),
-                        html.P(
-                            children=[
-                                html.B(_("Get neighbours")), " - ",
-                                html.Label(
-                                    _("Supplement the graph with tables that relate directly to the selected tables")
-                                ),
-                            ]
-                        ),
-                        html.P(
-                            children=[
-                                html.B(_("Get info about columns of selected tables")), " - ",
-                                html.Label(
-                                    _("Displays information about the columns of the selected table(s)")
-                                ),
-                            ]
-                        ),
-                        html.P(
-                            children=[
-                                html.B(_("Get info on displayed tables")), " - ",
-                                html.Label(
-                                    _("Displays the information of the tables shown in the graph")
-                                ),
-                            ]
-                        ),
+                        html.P([
+                            html.B(_("Graph engine")), ", ", html.B(_("Layout")), " - ",
+                            html.Label(_("Style of the point placement in the graph.")),
+                        ]),
+                        html.P([
+                            html.B(_("Select tables to graph")), " - ",
+                            html.Label(
+                                _("Select tables which relationships you want to display.")
+                            ),
+                        ]),
+                        html.P([
+                            html.B(_("Add list of tables")), " - ",
+                            html.Label([
+                                _("You can specify the tables to be displayed as the list; "
+                                  "the names of the tables must be separated by commas (space is optional). "),
+                                _("This input field also support wildcard characters:"),
+                                html.Ul([
+                                    html.Li([html.B("*"), " ", _("matches zero or more of any character;")]),
+                                    html.Li([html.B("?"), " ", _("matches exactly one character.")]),
+                                ]),
+                            ]),
+                        ]),
+                        html.P([
+                            html.B(_("Get neighbours")), " - ",
+                            html.Label(
+                                _("Supplement the graph with tables that directly relate to the selected tables; "
+                                  "neighbors will be shown with a gray background.")
+                            ),
+                        ]),
+                        html.P([
+                            html.B(_("Get info about columns of selected tables")), " - ",
+                            html.Label(
+                                _("Below the graph, displays information about the columns of the selected table(s).")
+                            ),
+                        ]),
+                        html.P([
+                            html.B(_("Get info on displayed tables")), " - ",
+                            html.Label(
+                                _("Below the graph, displays the information of the tables drawn in the graph.")
+                            ),
+                        ]),
                     ],
                     className="card-text",
                 ),
@@ -144,7 +150,7 @@ def filters_usage_info():
                 target="tutorial-filtrai-legacy-target",
                 body=True,
                 trigger="legacy",
-                style={"fontSize": "90%"},
+                style={"fontSize": "90%", "minWidth": "450px"},
                 placement="left-start",
             ),
         ]
